0064ce44bd48e95bfd93926905d590f41ec4d671
hamedtarkashvand <hamed.tarkashvand@gmail.com>
2023-05-19 22:38:33 +0330

    clean code

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import QuestionInput from '@/components/QuestionInput';
 import WrapperAboutAndMessages from '@/components/WrapperAboutAndMessages';
 export default function Home() {
   return (
-    <main className='flex  h-screen max-h-screen min-h-full flex-col items-center justify-center overflow-hidden p-5'>
+    <main className='flex h-screen max-h-screen min-h-full flex-col items-center justify-center overflow-hidden p-5'>
       <WrapperAboutAndMessages />
       <QuestionInput />
     </main>
